89907bdde2eba897360cf777a261bb838bd4ad41
geoliak <g.liakopoulos@hotmail.com>
2017-01-09 18:00:03 +0100

    j'avais oublie display.h

---
+++ display.h
@@ -0,0 +1,22 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "util.h"
+#include "elf.h"
+#include "readelf.h"
+
+
+void displayNameSection(ElfFile elf1);
+void displaySection(ElfFile elf1);
+void displayHeaderInfos(ElfFile elf1);
+void displaySymbolTable(ElfFile elf1);
+void displayRelocatableTable(ElfFile elf1);
+void displayMenu(FILE* ElfFile);
+
+#endif
